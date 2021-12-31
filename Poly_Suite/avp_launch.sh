e4852bf2c41b0ae985f07269128d761e3b7ba5dd
Sneha <59361783+Sneha783@users.noreply.github.com>
2021-12-31 15:53:21 +0530

    Delete Poly_Suite directory

--- Poly_Suite/avp_launch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Running AVP Demo"
-./support_files/avp_setup.sh
--- Poly_Suite/support_files/path_planner_validate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Start AD Path Planner Validation "
-
-gnome-terminal --tab --title="Path Planner Validation" --command="bash -c 'cd ../Poly_Suite; python3 pathPlannerCalculation/pathPlannerValidation.py; echo Report Generated > ../Poly_Suite/logfiles/logInfo.txt; $SHELL'"
-
-
-
