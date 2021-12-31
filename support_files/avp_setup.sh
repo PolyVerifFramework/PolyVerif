767828c720769efa8f32646eb764ba4a6fcb6039
Sneha <59361783+Sneha783@users.noreply.github.com>
2021-12-31 15:59:15 +0530

    Delete support_files directory

--- support_files/avp_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-#gnome-terminal --tab --title="AVP Demo" --command="bash -c 'cd ../AutowareAuto; ade enter ./PolyVerif_Shell/support_files/launch_avp.sh; exit; $SHELL'"
-
-
-gnome-terminal --tab --title="AVP Demo" --command="bash -c 'cd ../AutowareAuto; ade enter ./PolyVerif_Shell/support_files/launch_avp.sh; $SHELL'"
