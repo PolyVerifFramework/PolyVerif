1b3a3aaf05ccf9f840f1a81d49cc082e9a5eff08
chavankunal <ikunalchavan@gmail.com>
2024-05-29 12:24:43 +0530

    build_env updted

--- build_env.sh
+++ build_env.sh
@@ -7,7 +7,10 @@ sudo apt-get install zenity
 sudo apt install python3
 sudo apt install python3-pip
 pip install pandas
-
+sudo apt install wget
+sudo wget https://gitlab.com/ApexAI/ade-cli/uploads/85a5af81339fe55555ee412f9a3a734b/ade+x86_64 -P /usr/local/bin
+sudo mv /usr/local/bin/ade+x86_64 /usr/local/bin/ade
+sudo chmod +x /usr/local/bin/ade
 
 # Start ADE within a new GNOME Terminal tab
 gnome-terminal --tab --title="ADE Start" --command="bash -c 'cd adehome/AutowareAuto;  ade start -- --net=host --privileged; echo ADE Started, Now Entering into Docker.. > ../Poly_Suite/logfiles/logInfo.txt; sleep 1; ade enter ./polyverif_package.sh; echo Done; zenity  --width=300  --info --text=\"Polyverif Setup Completed\"; $SHELL'"
