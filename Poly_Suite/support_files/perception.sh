5ccd99dd7b0498c7550c3f92837ab690669496a6
kunal <104835315+chavankunal@users.noreply.github.com>
2025-01-27 17:53:11 +0530

    Update perception.sh

--- Poly_Suite/support_files/perception.sh
+++ Poly_Suite/support_files/perception.sh
@@ -5,4 +5,4 @@ echo "Starting Autoware_Perception"
 #source ros2-lgsvl-bridge/install/setup.bash 
 #ros2 launch autoware_demos lidar_bounding_boxes_lgsvl.launch.py 
 
-ros2 launch autoware_launch e2e_simulator.launch.xml vehicle_model:=sample_vehicle sensor_model:=awsim_sensor_kit map_path:=$HOME/autoware_map/nishishinjuku_autoware_map/
+ros2 launch autoware_launch e2e_simulator.launch.xml vehicle_model:=sample_vehicle sensor_model:=awsim_sensor_kit map_path:=$HOME/adehome/nishishinjuku_autoware_map/
