e932d46f9f001f6c42624d2b900b951bc84c7828
kunal <104835315+chavankunal@users.noreply.github.com>
2025-03-10 11:11:42 +0530

    Update perception.sh

--- Poly_Suite/support_files/perception.sh
+++ Poly_Suite/support_files/perception.sh
@@ -2,6 +2,6 @@
 
 echo "Starting Autoware_Perception"
 
-ros2 launch autoware_launch e2e_simulator.launch.xml vehicle_model:=sample_vehicle sensor_model:=awsim_sensor_kit map_path:=autoware_map/nishishinjuku_autoware_map/
+ros2 launch autoware_launch e2e_simulator.launch.xml vehicle_model:=sample_vehicle sensor_model:=awsim_sensor_kit map_path:=nishishinjuku_autoware_map/
 
 echo "Autoware Perception Stack Started"
